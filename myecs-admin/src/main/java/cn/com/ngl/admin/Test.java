fbbefc01e1ea139e512a754499137d231fbecc02
ngl1125 <naguoliang@163.com>
2016-05-07 13:48:39 +0800

    ngl commit

---
+++ myecs-admin/src/main/java/cn/com/ngl/admin/Test.java
@@ -0,0 +1,7 @@
+package cn.com.ngl.admin;
+
+/**
+ * Created by NGL on 2016/5/7.
+ */
+public class Test {
+}
---
+++ myecs-online/src/main/java/cn/com/ngl/online/Test.java
@@ -0,0 +1,7 @@
+package cn.com.ngl.online;
+
+/**
+ * Created by NGL on 2016/5/7.
+ */
+public class Test {
+}
